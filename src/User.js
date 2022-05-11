a9ece28b5986cf58f70c3288befc3ce98db4af7f
prodigl <35952708+prodigl@users.noreply.github.com>
2022-05-11 10:55:15 +0530

    update commit

---
+++ src/User.js
@@ -0,0 +1,9 @@
+const User = ()=> {
+    return (
+        <>
+            <h1>Welcome</h1>
+        </>
+    )
+}
+
+export default User; 
---
+++ src/components/Home.js
@@ -0,0 +1,165 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div >
+        <h1>Home Component</h1>
+        <div className='product-design'>
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src="https://m.media-amazon.com/images/I/51OYalGc88L._SY741_.jpg" />
+                </div>
+                <div className='name-wrapper item'>
+                    <span>
+                        IQOO
+                    </span>
+                    <span>
+                    Price : $30,000
+                    </span>
+                </div>
+                <div className='button-wrapper item'>
+                    <button>
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src="https://m.media-amazon.com/images/I/51OYalGc88L._SY741_.jpg" />
+                </div>
+                <div className='name-wrapper item'>
+                    <span>
+                        IQOO
+                    </span>
+                    <span>
+                    Price : $30,000
+                    </span>
+                </div>
+                <div className='button-wrapper item'>
+                    <button>
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src="https://m.media-amazon.com/images/I/51OYalGc88L._SY741_.jpg" />
+                </div>
+                <div className='name-wrapper item'>
+                    <span>
+                        IQOO
+                    </span>
+                    <span>
+                    Price : $30,000
+                    </span>
+                </div>
+                <div className='button-wrapper item'>
+                    <button>
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src="https://m.media-amazon.com/images/I/51OYalGc88L._SY741_.jpg" />
+                </div>
+                <div className='name-wrapper item'>
+                    <span>
+                        IQOO
+                    </span>
+                    <span>
+                    Price : $30,000
+                    </span>
+                </div>
+                <div className='button-wrapper item'>
+                    <button>
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src="https://m.media-amazon.com/images/I/51OYalGc88L._SY741_.jpg" />
+                </div>
+                <div className='name-wrapper item'>
+                    <span>
+                        IQOO
+                    </span>
+                    <span>
+                    Price : $30,000
+                    </span>
+                </div>
+                <div className='button-wrapper item'>
+                    <button>
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src="https://m.media-amazon.com/images/I/51OYalGc88L._SY741_.jpg" />
+                </div>
+                <div className='name-wrapper item'>
+                    <span>
+                        IQOO
+                    </span>
+                    <span>
+                    Price : $30,000
+                    </span>
+                </div>
+                <div className='button-wrapper item'>
+                    <button>
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src="https://m.media-amazon.com/images/I/51OYalGc88L._SY741_.jpg" />
+                </div>
+                <div className='name-wrapper item'>
+                    <span>
+                        IQOO
+                    </span>
+                    <span>
+                    Price : $30,000
+                    </span>
+                </div>
+                <div className='button-wrapper item'>
+                    <button>
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+
+            <div className='cart-wrapper'>
+                <div className='img-wrapper item'>
+                    <img src="https://m.media-amazon.com/images/I/51OYalGc88L._SY741_.jpg" />
+                </div>
+                <div className='name-wrapper item'>
+                    <span>
+                        IQOO
+                    </span>
+                    <span>
+                    Price : $30,000
+                    </span>
+                </div>
+                <div className='button-wrapper item'>
+                    <button>
+                        Add To Card
+                    </button>
+                </div>
+            </div>
+           
+        </div>
+    </div>
+  )
+}
+
+export default Home
